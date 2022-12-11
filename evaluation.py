d34bb0d065b1a99d0a64686479cdd03b5fdca72b
Yicong-Li <jackson_li_0225@163.com>
2022-12-10 22:11:38 -0500

    add docker container for evaluation

---
+++ evaluation.py
@@ -0,0 +1,95 @@
+import daisy
+import time
+import json
+import os.path
+import networkx as nx
+import numpy as np
+import argparse
+from funlib import evaluate
+from evalutils.evalutils import (
+    DEFAULT_INPUT_PATH,
+    DEFAULT_EVALUATION_OUTPUT_FILE_PATH,
+    DEFAULT_GROUND_TRUTH_PATH,
+)
+
+def assign_skeleton_indexes(graph):
+    '''Assign unique ids to each cluster of connected nodes. This is to
+    differentiate between sets of nodes that are discontinuous in the
+    ROI but actually belong to the same skeleton ID, which is necessary
+    because the network should not be penalized for incorrectly judging
+    that these processes belong to different neurons.'''
+    skeleton_index_to_id = {}
+    skel_clusters = nx.connected_components(graph)
+    for i, cluster in enumerate(skel_clusters):
+        for node in cluster:
+            graph.nodes[node]['skeleton_index'] = i
+        skeleton_index_to_id[i] = graph.nodes[cluster.pop()]['skeleton_id']
+    return graph
+
+
+def add_predicted_seg_labels_from_vol(
+        graph, segmentation_file, segmentation_ds,
+        load_segment_array_to_memory=True):
+
+    bg_nodes=0
+    segment_array = daisy.open_ds(segmentation_file, segmentation_ds)
+    segment_array = segment_array[segment_array.roi]
+    if load_segment_array_to_memory:
+        segment_array.materialize()
+
+    nodes_outside_roi = []  
+    for i, (treenode, attr) in enumerate(graph.nodes(data=True)):
+        pos = attr["position"]
+        try:    
+            attr['zyx_coord'] = (pos[2], pos[1], pos[0])
+            attr['seg_label'] = segment_array[daisy.Coordinate(attr['zyx_coord'])]
+            if attr['seg_label'] == 0:
+                bg_nodes+=1
+                raise AssertionError
+        except AssertionError as e:
+            nodes_outside_roi.append(treenode)
+
+    print(f'Removing {len(nodes_outside_roi)} GT annotations outside of evaluated ROI')
+    for node in nodes_outside_roi:
+        graph.remove_node(node)
+
+    print('BG_NODES',bg_nodes)
+    return assign_skeleton_indexes(graph)
+
+
+def generate_graphs_with_seg_labels(segmentation_file, segmentation_ds, skeleton_path, num_processes):
+    unlabeled_skeleton = np.load(skeleton_path, allow_pickle=True)
+    return add_predicted_seg_labels_from_vol(unlabeled_skeleton.copy(), segmentation_file, segmentation_ds)    
+
+
+def eval_erl(skeleton_file, segmentation_file, segmentation_ds):
+    
+    node_seg_lut = {}
+    graph_list = generate_graphs_with_seg_labels(segmentation_file, segmentation_ds, skeleton_file, 1)
+    for node, attr in graph_list.nodes(data=True):
+        node_seg_lut[node]=attr['seg_label']
+
+    res = evaluate.expected_run_length(skeletons=graph_list,skeleton_id_attribute='skeleton_id',
+                        node_segment_lut=node_seg_lut,skeleton_position_attributes=['zyx_coord'],
+                        return_merge_split_stats=False,edge_length_attribute='edge_length')
+
+    return res
+
+
+class XPRESS:
+    def __init__(self):
+        self.input_file = os.path.join(DEFAULT_INPUT_PATH, "test_unet_vxel_skel_300_frag06_expanded.h5")
+        self.output_file = DEFAULT_EVALUATION_OUTPUT_FILE_PATH
+        self.gt_file = os.path.join(DEFAULT_GROUND_TRUTH_PATH, "100nm_Cutout6_Testing_1025.npz")
+        self.segmentation_ds = 'submission'
+
+    def evaluate(self):
+
+        metrics = {"Expected run-length": eval_erl(self.gt_file, self.input_file, self.segmentation_ds)}
+
+        with open(self.output_file, "w") as f:
+            f.write(json.dumps(metrics))
+
+
+if __name__ == "__main__":
+    XPRESS().evaluate()
